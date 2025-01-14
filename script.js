29a04c4391b858ada940a9f5bb66158ca2b7fecc
rishabhgarai <141753696+rishabhgarai@users.noreply.github.com>
2025-01-15 04:49:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('copy-btn').addEventListener('click', () => {
+    const apiInput = document.getElementById('api-url');
+    apiInput.select();
+    apiInput.setSelectionRange(0, 99999); // For mobile devices
+    
+});
+
+function updateApi() {
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    const apiInput = document.getElementById('api-url');
+    apiInput.value = `https://flood-api-mbrishabh.onrender.com/weather?latitude=${latitude}&longitude=${longitude}`;
+}
